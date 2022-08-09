e8789bfdbd5dd8df1892b77de6551f301f504f5f
Byron Murray <byron4stacey@hotmail.com>
2022-08-10 10:59:56 +1200

    init project tidy files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,27 @@ import { Container } from 'react-bootstrap'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from './screens/Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Product from './screens/Product';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
         <main className='py-3'>
           <Container>
-            <Home />
+            <Routes>
+              <Route exact  path='/' element={<Home />} />
+              <Route path='/product/:id' element={<Product />} />
+            </Routes>
           </Container>
         </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
