85eb27062d598d6ffe870346f37957aa242a60f1
Byron Murray <byron4stacey@hotmail.com>
2022-08-11 10:13:25 +1200

    fetching products

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import products from '../products'
+import React, {useState} from 'react'
+import { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from './../components/Product';
+import axios from 'axios'
+
 
 const Home = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, [] )
+
   return (
     <>
         <h2>Latest Products</h2>
--- frontend/src/screens/Product.jsx
+++ frontend/src/screens/Product.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useEffect } from 'react';
 import { Link, useParams  } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from './../components/Rating';
-import products from '../products';
+import axios from 'axios'
 
 const Product = () => {
   const { id } = useParams()
-  const product = products.find(product => product._id === id )
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/product/${id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [] )
 
   return (
     <>
