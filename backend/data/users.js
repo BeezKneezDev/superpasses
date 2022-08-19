4b1084fe9bf6340f0c8911e6dc3bca8282c52bd7
Byron Murray <byron4stacey@hotmail.com>
2022-08-20 10:10:19 +1200

    auth users routes and models

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,22 +1,22 @@
 import bcrypt from 'bcryptjs'
 
 const users = [
-    {
-        name: 'Admin User',
-        email: 'admin@example',
-        password: bcrypt.hashSync('123456', 10),
-        isAdmin: true
-    },
-    {
-        name: 'Joe Doe',
-        email: 'joe@example',
-        password: bcrypt.hashSync('123456', 10)
-    },
-    {
-        name: 'Jane Doe',
-        email: 'jane@example',
-        password: bcrypt.hashSync('123456', 10)
-    },
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true
+  },
+  {
+    name: 'Joe Doe',
+    email: 'joe@example.com',
+    password: bcrypt.hashSync('123456', 10)
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 10)
+  }
 ]
 
 export default users
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,27 +1,38 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
+import bcrpt from 'bcryptjs'
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     isAdmin: {
-        type: String,
-        required: true,
-        default: false
-    },
-}, {
+      type: String,
+      required: true,
+      default: false
+    }
+  },
+  {
     timestamps: true
-} )
+  }
+)
+
+// run after a email match is found
+// enteredpassword ~ userInput
+// this.password ~ checking against the password in the DB
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrpt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
