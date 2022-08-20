acf3a23affeedd8c51ed4e3c00c946b4ea6a72ce
Byron Murray <byron4stacey@hotmail.com>
2022-08-20 14:28:40 +1200

    jwt tokens middeware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 // @des     Auth user & get token
@@ -16,7 +17,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null
+      token: generateToken(user._id)
     })
   } else {
     res.status(401)
@@ -24,4 +25,20 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser }
+// @des     Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  }
+})
+
+export { authUser, getUserProfile }
